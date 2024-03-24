1c27d86a73f23004f90afc418625d666064e296e
petramilenium <161418499+petramilenium@users.noreply.github.com>
2024-03-24 13:15:27 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-e7c6b702-562c-4b34-846f-338a228671d5/)
 
 
 echo "$curl_output" >> Update.tsx
